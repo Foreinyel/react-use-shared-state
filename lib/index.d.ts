8430c1edfc5539ef6eb9864481c130b03769a6cd
jimmy.shi <jimmy.shi@58.com>
2021-09-17 18:05:03 +0800

    use pub state

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,5 +1,6 @@
 declare type StateName = any;
 declare type StateValue = any;
+export declare const usePubState: (stateName: StateName) => ((newValue: StateValue) => void)[];
 export declare const useSharedState: (stateName: StateName, initValue?: StateValue) => any[];
 export declare const getSharedState: (stateName: StateName) => any;
 export default useSharedState;
