a4528e9d7d3752ab1f09093f9ce3d59008403a17
Foreinyel <Foreinyel@163.com>
2022-06-15 15:30:59 +0800

    update use pub state

--- src/index.ts
+++ src/index.ts
@@ -64,7 +64,7 @@ const setState: (stateName: StateName, newValue: StateValue) => void = (
   StatesMap.set(stateName, initValue);
 };
 
-export const usePubState = (stateName: StateName) => {
+export const usePubState = (stateName: StateName, initValue?: StateValue) => {
   const [uuid] = useState(Symbol(stateName));
 
   const subject = getSubject(stateName);
@@ -76,6 +76,12 @@ export const usePubState = (stateName: StateName) => {
       uuid,
     });
   };
+
+  useEffect(() => {
+    if (initValue) {
+      publishValue(initValue);
+    }
+  }, []);
 
   return [publishValue];
 };
